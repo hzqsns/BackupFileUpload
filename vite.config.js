6946dee5759a69535120326a1b5467ef267d3254
hzqsns <787624681@qq.com>
2025-03-24 16:15:27 +0800

    feat: 修改vite配置,增加ip启动方式

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import { createVuePlugin } from 'vite-plugin-vue2'
 export default defineConfig({
   plugins: [createVuePlugin()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
